5bffcce9b332d9992cf7088bc21fdf487dfe5a3d
A-Vershinin <cata07@yandex.ru>
2019-06-20 17:50:47 +0300

    Create Registration page

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -1,13 +1,91 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Registration page</h1>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md6>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Registration form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form ref="form" v-model="valid" lazy-validation>
+              <v-text-field
+                prepend-icon="person"
+                name="email"
+                label="Email"
+                type="email"
+                v-model="email"
+                :rules="emailRules"
+                required
+              ></v-text-field>
+
+              <v-text-field
+                prepend-icon="lock"
+                name="password"
+                label="Password"
+                type="password"
+                v-model="password"
+                :rules="passwordRules"
+                :counter="6"
+                required
+              ></v-text-field>
+
+              <v-text-field
+                prepend-icon="lock"
+                name="configm-password"
+                label="Confirm Password"
+                type="password"
+                v-model="confirmPassword"
+                :rules="confirmPasswordRules"
+                :counter="6"
+                required
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" :disabled="!valid" @click="onSubmit">Create account</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-export default {};
+const emailRegexp = /.+@.+/;
+
+export default {
+  props: { source: String },
+  data() {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      valid: false,
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => emailRegexp.test(v) || 'E-mail must be valid',
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 6) || 'Password must be more than 6 characters',
+      ],
+      confirmPasswordRules: [
+        v => !!v || 'Password is required',
+        v => v === this.password || 'Password should match',
+      ],
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (this.$refs.form.validate()) {
+        const user = {
+          email: this.email,
+          password: this.password,
+        };
+        console.log('Login onSubmit:', user);
+      }
+    },
+  },
+};
 </script>
