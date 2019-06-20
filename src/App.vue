e8bf3b5e34298fdfde90b44a456df6717681af16
A-Vershinin <cata07@yandex.ru>
2019-06-20 14:05:54 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app temporary></v-navigation-drawer>
+    <v-toolbar app dark color="primary">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>Advertising App</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>Link One</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    <v-footer app></v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
