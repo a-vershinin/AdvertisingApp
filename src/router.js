905ca2c1abc040b5573ef43816b54e8ff5bef8de
A-Vershinin <cata07@yandex.ru>
2019-06-20 17:14:50 +0300

    Create array of routes

--- src/router.js
+++ src/router.js
@@ -1,6 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import { Home } from '@/components';
+import {
+  Home,
+  Login,
+  Registration,
+  Orders,
+  AdList,
+  AdNew,
+  AdItem,
+} from '@/components';
 
 Vue.use(Router);
 
@@ -10,7 +18,38 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Home',
       component: Home,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+    },
+    {
+      path: '/registration',
+      name: 'Reg',
+      component: Registration,
+    },
+    {
+      path: '/list',
+      name: 'AdList',
+      component: AdList,
+    },
+    {
+      path: '/ad/:id',
+      name: 'AdItem',
+      component: AdItem,
+    },
+    {
+      path: '/new',
+      name: 'AdNew',
+      component: AdNew,
+    },
+    {
+      path: '/orders',
+      name: 'Orders',
+      component: Orders,
     },
   ],
 });
