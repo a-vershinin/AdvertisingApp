f9bba5a28f578a51ffa5f3563c23fa656050f437
A-Vershinin <cata07@yandex.ru>
2019-06-21 15:19:23 +0300

    Create advertising list page

--- src/components/AdList.vue
+++ src/components/AdList.vue
@@ -1,13 +1,47 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>AdList page</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">My advertisements</h1>
+
+        <v-card class="elevation-10 mb-3" v-for="ad of ads" :key="ad.id">
+          <v-layout row wrap>
+            <v-flex xs4>
+              <v-img :src="ad.imageSrc" height="134" />
+            </v-flex>
+
+            <v-flex xs8>
+              <v-card-text>
+                <h2 class="text--primary">{{ ad.title }}</h2>
+                <p>{{ ad.description }}</p>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="info" :to="'/ad/' + ad.id">Open</v-btn>
+              </v-card-actions>
+            </v-flex>
+          </v-layout>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      ads: [
+        {
+          id: '1',
+          title: 'First ad',
+          desc: 'Hello I am description of first Ad',
+          promo: false,
+          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+        },
+      ],
+    };
+  },
+};
 </script>
