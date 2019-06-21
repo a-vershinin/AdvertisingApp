988b124fce3accf1bf080ca736f26775751b368c
A-Vershinin <cata07@yandex.ru>
2019-06-21 14:34:19 +0300

    Create New advertising page

--- src/components/AdNew.vue
+++ src/components/AdNew.vue
@@ -1,13 +1,95 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>AdNew page</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Create new advertising</h1>
+
+        <v-form ref="form" v-model="valid" validation>
+          <v-text-field
+            name="title"
+            label="Add title"
+            type="text"
+            class="mb-4"
+            v-model="title"
+            :rules="[v => !!v || 'Title is required']"
+            required
+          ></v-text-field>
+          <v-textarea
+            type="text"
+            name="description"
+            label="Add description"
+            class="mb-4"
+            v-model="description"
+            :rules="[v => !!v || 'Description is required']"
+            required
+            multi-line
+          >
+          </v-textarea>
+        </v-form>
+
+        <v-layout row>
+          <v-flex xs12>
+            <v-btn
+              :loading="loading"
+              :disabled="loading"
+              color="blue-grey mb-4"
+              class="warning white--text"
+            >
+              Upload image
+              <v-icon right dark>cloud_upload</v-icon>
+            </v-btn>
+          </v-flex>
+        </v-layout>
+
+        <v-layout row>
+          <v-flex xs12>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+              height="100"
+              aspect-ratio="2.75"
+            ></v-img>
+          </v-flex>
+        </v-layout>
+
+        <v-layout row>
+          <v-flex xs12>
+            <v-switch label="Add to Promo?" color="primary" v-model="promo"></v-switch>
+          </v-flex>
+        </v-layout>
+
+        <v-layout row>
+          <v-flex xs12>
+            <v-spacer></v-spacer>
+            <v-btn color="success" :disabled="!valid" @click="createAd">Create advertising</v-btn>
+          </v-flex>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      title: '',
+      description: '',
+      promo: false,
+      valid: false,
+      loading: false,
+    };
+  },
+  methods: {
+    createAd() {
+      if (this.$refs.form.validate()) {
+        const adItem = {
+          title: this.title,
+          description: this.description,
+          promo: this.promo,
+        };
+        console.log('createAd onSubmit:', adItem);
+      }
+    },
+  },
+};
 </script>
